e505273bb8a5ccd5e561d709450f70650091dd14
William M <wmooney1984@gmail.com>
2020-02-07 04:37:12 -0500

    Section 4: Completed

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
+import HomePage from './pages/homepage/homepage.component';
 
 class App extends Component {
   render() {
-    return <div>Hello World</div>;
+    return <HomePage />
   }
 }
 
