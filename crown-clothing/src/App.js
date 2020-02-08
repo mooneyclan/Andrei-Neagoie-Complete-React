73913b9c1fa538bbbcd8ce2c98e46232cdd7fe4f
William M <wmooney1984@gmail.com>
2020-02-07 23:40:12 -0500

    Section 5: Completed

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 class App extends Component {
   render() {
-    return <HomePage />
+    return (
+      <div>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/hats' component={HatsPage} />
+        </Switch>
+      </div>
+    )
   }
 }
 
